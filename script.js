53011a362372a57e479f37abb66b3e2e3bb9ea02
Code-Rooster <codingroosters@gmail.com>
2022-02-11 18:54:06 -0800

    0.0.7
    // working dropdown

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+//$("#feelings-dropdown").empty();
+
+const jsonFeelings = JSON.parse(`[ 
+  { "feeling":"angry" }, 
+  { "feeling":"frustrated" }, 
+  { "feeling":"tired" }, 
+  { "feeling":"confused" }, 
+  { "feeling":"discombobulated" }, 
+  { "feeling":"distraught" }, 
+  { "feeling":"misunderstood" }, 
+  { "feeling":"hurt" }]`);
+
 $(document).ready(function()
 {
-  $('.dropdown').select2();
-});
+  $.each(jsonFeelings, function (key, entry) {
+    console.log(entry);
+    $("#feelings-dropdown").append($('<option value="' + entry.feeling + '">' + entry.feeling + '</option>'));
+  });
+})
